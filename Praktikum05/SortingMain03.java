c5dff836d69846c3e36f05a4eb038e2fcebfaecc
akmalfitrahari-sib <akmal.fitrahari@gmail.com>
2026-04-07 10:17:49 +0700

    Sorting Main v3

--- Praktikum05/SortingMain03.java
+++ Praktikum05/SortingMain03.java
@@ -6,6 +6,9 @@ public class SortingMain03 {
         int b[] = {30, 20, 2, 8, 14};
         Sorting03 dataurut2 = new Sorting03(b, b.length);
 
+        int c[] = {40, 10, 4, 9, 3};
+        Sorting03 dataurut3 = new Sorting03(c, c.length);
+
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort(); 
@@ -17,6 +20,12 @@ public class SortingMain03 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC");
         dataurut2.tampil();
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC");
+        dataurut3.tampil();
     }
     
 }
